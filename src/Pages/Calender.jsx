9bbd1297c5bd74421dc96f5297c02ea2a5dc5261
Rahulzhp <akumari42872@gmail.com>
2022-12-19 15:02:17 +0530

    m

--- src/Pages/Calender.jsx
+++ src/Pages/Calender.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import GitHubCalendar from "react-github-calendar";
 import { Row } from "react-bootstrap";
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Text,Image } from "@chakra-ui/react";
 
 function Calender() {
   return (
     <Box bg="linear-gradient(180deg, rgba(44,23,75,1) 45%, rgba(28,14,50,1) 99%)" margin="auto" color="white" pb="60px">
         <Box w="80%" transform="scale(1.05)" m="auto" textAlign="center" p="60px auto">
             <Row style={{ justifyContent: "center", padding: "20px auto", paddingTop: "60px"}}>
-            <Text margin="auto" textAlign="center"  fontWeight="400" fontSize={["3xl", "4xl", "5xl"]}>My Github Calender</Text>
+            <Text margin="auto" textAlign="center"  fontWeight="400" fontSize={["xl", "xl", "4xl"]}>My Github Calender</Text>
             <GitHubCalendar 
                 margin="auto"
                 username="Rahulzhp"
@@ -18,6 +18,14 @@ function Calender() {
                 fontSize={15}
             />
             </Row>
+            <Box display={["inline","inline","flex"]}  mt={["50px","55px","45px"]} m="auto" >
+            <Box m="auto" p="12px 3px" w={["90%","80%","48%"]}  >
+              <Image w="100%" src="https://github-readme-stats.vercel.app/api?username=Rahulzhp&theme=dark&show_icons=true&count_private=true"></Image>
+            </Box>
+            <Box m="auto" p="12px 3px" w={["90%","80%","48%"]}>
+              <Image w="100%"  src="https://github-readme-streak-stats.herokuapp.com/?user=Rahulzhp&theme=dark"></Image>
+            </Box>
+            </Box>
         </Box>
     </Box>
   );
