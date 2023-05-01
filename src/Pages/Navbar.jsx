5b6b0e9c34d4d3860ad1faba2721b88507296916
Rahulzhp <akumari42872@gmail.com>
2023-05-01 09:41:01 +0530

    addedClass

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -35,7 +35,7 @@ function Navbar() {
             "noopener,noreferrer"
         )
     }
-    return <Box id="nav-menu" zIndex={1} color={isscroll ? darkTheme === "dark" ? "white" : "white" : darkTheme === "dark" ? "white" : "white"} bg={isscroll ? darkTheme === "dark" ? "#48B3E9 " : "#48B3E9 " : darkTheme === "dark" ? "#1D2143" : "#1D2143"} w="100%" p="11px 9px" boxShadow="md" position="fixed">
+    return <Navbar id="nav-menu" zIndex={1} color={isscroll ? darkTheme === "dark" ? "white" : "white" : darkTheme === "dark" ? "white" : "white"} bg={isscroll ? darkTheme === "dark" ? "#48B3E9 " : "#48B3E9 " : darkTheme === "dark" ? "#1D2143" : "#1D2143"} w="100%" p="11px 9px" boxShadow="md" position="fixed">
         <Box w="91%" m="auto" display={["flex", "flex", "flex"]} justifyContent="space-between" textAlign={["center", "", ""]}>
             <Box >
                 <Text fontFamily="inherit" fontWeight="500" paddingLeft={["8px", "13px", "25px"]} fontSize={['lg', 'sm', '3xl']}>Rahul Das</Text>
@@ -46,48 +46,48 @@ function Navbar() {
             </Box >
             {/* <Text fontWeight="bold" ml={["15px", "2px", "10px"]} fontSize={['xl', '2xl', '3xl']}>Sanghamitra</Text> */}
             <Box id="rt" gap={["auto", "13px", "23px"]} color={isscroll ? darkTheme === "dark" ? "white" : "black" : darkTheme === "dark" ? "white" : "white"} justifyContent={["space-around", "space-between", "space-between"]} ml="10px" alignItems="center">
-                <Link display="flex" to="/">
+                <Link className="nav-link home" display="flex" to="/">
                     <AnchorLink href='#Home'><Text color={darkTheme === "dark" ? "gray" : "white"} fontFamily="inherit" fontWeight="500" fontSize={['sm', 'sm', 'lg']}>Home</Text></AnchorLink>
                 </Link>
-                <Link display="flex" to="/about">
+                <Link className="nav-link about" display="flex" to="/about">
                     <AnchorLink href='#About'><Text color={darkTheme === "dark" ? "gray" : "white"} fontFamily="inherit" fontWeight="500" fontSize={['sm', 'sm', 'lg']}>About</Text></AnchorLink>
                 </Link>
-                <Link display="flex" to="/skills">
+                <Link className="nav-link skills" display="flex" to="/skills">
                     <AnchorLink href='#Skills'><Text color={darkTheme === "dark" ? "gray" : "white"} fontFamily="inherit" fontWeight="500" fontSize={['sm', 'sm', 'lg']}>Skills</Text></AnchorLink>
                 </Link>
-                <Link display="flex">
+                <Link className="nav-link projects" display="flex">
                     <AnchorLink href='#Project'><Text color={darkTheme === "dark" ? "gray" : "white"} fontFamily="inherit" fontWeight="500" _active={{ textDecoration: "underlined" }} fontSize={['sm', 'sm', 'lg']}>Project</Text></AnchorLink>
                 </Link>
-                <Link display="flex">
+                <Link className="nav-link contact" display="flex">
                     <AnchorLink href='#Contact'><Text color={darkTheme === "dark" ? "gray" : "white"} fontFamily="inherit" fontWeight="500" fontSize={['sm', 'sm', 'lg']}>Contact</Text></AnchorLink>
                 </Link>
-                <Text onClick={handleClick} color={darkTheme === "dark" ? "gray" : "white"} fontFamily="inherit" fontWeight="500" fontSize={['sm', 'sm', 'lg']}><a href="https://drive.google.com/uc?export=download&id=1fHs0WXpCaHcAUypp7c8C1qIGgH1xVYuH">Resume</a></Text>
+                <Text className="nav-link resume" onClick={handleClick} color={darkTheme === "dark" ? "gray" : "white"} fontFamily="inherit" fontWeight="500" fontSize={['sm', 'sm', 'lg']}><a href="https://drive.google.com/uc?export=download&id=1fHs0WXpCaHcAUypp7c8C1qIGgH1xVYuH">Resume</a></Text>
             </Box>
 
 
         </Box>
         {
             ham ? <Box className="nt" gap={["auto", "13px", "23px"]} color={isscroll ? darkTheme === "dark" ? "white" : "black" : darkTheme === "dark" ? "white" : "white"} justifyContent={["space-around", "space-between", "space-between"]} ml="10px" alignItems="center">
-                <Link display="flex" to="/">
+                <Link className="nav-link home" display="flex" to="/">
                     <AnchorLink href='#Home'><Text color={darkTheme === "dark" ? "gray" : "white"} fontFamily="inherit" fontWeight="500" fontSize={['sm', 'sm', 'lg']}>Home</Text></AnchorLink>
                 </Link>
-                <Link display="flex" to="/about">
+                <Link className="nav-link about" display="flex" to="/about">
                     <AnchorLink href='#About'><Text color={darkTheme === "dark" ? "gray" : "white"} fontFamily="inherit" fontWeight="500" fontSize={['sm', 'sm', 'lg']}>About</Text></AnchorLink>
                 </Link>
-                <Link display="flex" to="/skills">
+                <Link className="nav-link skills" display="flex" to="/skills">
                     <AnchorLink href='#Skills'><Text color={darkTheme === "dark" ? "gray" : "white"} fontFamily="inherit" fontWeight="500" fontSize={['sm', 'sm', 'lg']}>Skills</Text></AnchorLink>
                 </Link>
-                <Link display="flex">
+                <Link className="nav-link projects" display="flex">
                     <AnchorLink href='#Project'><Text color={darkTheme === "dark" ? "gray" : "white"} fontFamily="inherit" fontWeight="500" _active={{ textDecoration: "underlined" }} fontSize={['sm', 'sm', 'lg']}>Project</Text></AnchorLink>
                 </Link>
-                <Link display="flex">
+                <Link className="nav-link contact" display="flex">
                     <AnchorLink href='#Contact'><Text color={darkTheme === "dark" ? "gray" : "white"} fontFamily="inherit" fontWeight="500" fontSize={['sm', 'sm', 'lg']}>Contact</Text></AnchorLink>
                 </Link>
-                <Text color={darkTheme === "dark" ? "gray" : "white"} fontFamily="inherit" fontWeight="500" fontSize={['sm', 'sm', 'lg']}><a href="https://drive.google.com/uc?export=download&id=1fHs0WXpCaHcAUypp7c8C1qIGgH1xVYuH">Resume</a></Text>
+                <Text className="nav-link resume" color={darkTheme === "dark" ? "gray" : "white"} fontFamily="inherit" fontWeight="500" fontSize={['sm', 'sm', 'lg']}><a href="https://drive.google.com/uc?export=download&id=1fHs0WXpCaHcAUypp7c8C1qIGgH1xVYuH">Resume</a></Text>
             </Box> : ""
         }
 
-    </Box>
+    </Navbar>
 }
 
 export default Navbar;
