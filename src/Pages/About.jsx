c685a025ad6ac0ed61e3ea775334f0d6beee7fee
Rahulzhp <akumari42872@gmail.com>
2023-05-08 13:17:27 +0530

    added id on intro

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -7,7 +7,7 @@ function About() {
             <Image w={["90%", "80%", "50%"]} m="auto" mt={["0px", "30px", "auto"]} src="https://github.com/Codex-Swapnil1/PortFolio/raw/main/Full-Stack-Development-Featured-Image-LevelFive-Solutions.gif" alt="img" />
             <Box w={["90%", "80%", "46%"]} m="auto">
                 <Text textAlign="center" fontWeight="400" mt="7px" fontSize={["3xl", "4xl", "5xl"]}>About Me</Text>
-                <Text fontFamily="inherit" textAlign="justify" mt="10px" fontSize={["13px", "15px", "17px"]}>
+                <Text id="user-detail-intro" fontFamily="inherit" textAlign="justify" mt="10px" fontSize={["13px", "15px", "17px"]}>
                     A self-motivated, hard-working, and ambitious Full-Stack web developer with proficiency in JavaScript, React, TypeScript, MongoDB, HTML and CSS.
                     I have made some solo and collaborative projects and continue to have some experience as well as acquiring more knowledge.
                     Looking forward to working as an accountable and competent employee in an exciting Tech company.
